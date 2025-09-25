9fb97c5b5cdf1008f3309bead3ee9b366b49e1ac
FrankVevle <ceo@cortavia.com>
2025-09-25 20:58:44 +0200

    codex 5.0.1 (del): health endpoint + users grants for RLS

--- db/migrations/232_users_grants.sql
+++ db/migrations/232_users_grants.sql
@@ -1,3 +1,15 @@
+-- RLS-verifisering: gi app-klienten lesetilgang til public.users (tilpass rolle)
+DO $$
+BEGIN
+  BEGIN
+    EXECUTE 'GRANT SELECT ON TABLE public.users TO app_client';
+  EXCEPTION WHEN undefined_table THEN
+    RAISE NOTICE 'public.users finnes ikke i dette skjemaet (hopper over)';
+  WHEN invalid_grant_operation THEN
+    RAISE NOTICE 'Rolle app_client finnes ikke (hopper over)';
+  END;
+END$$;
+
 -- 232_users_grants.sql
 -- Gi RLS-klientrollen nødvendige tabellprivilegier på public.users
 
